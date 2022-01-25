7494acf32506ea7a4356c0708bb5f3e8089f6978
Dushyant Pratap Singh <singhdushyant2901@gmail.com>
2022-01-25 23:02:57 +0530

    till search functionality

--- src/Movies.js
+++ src/Movies.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useGlobalContext } from "./context";
 
 const Movies = () => {
-  return <div>hi from the movies</div>;
+  const { movies, loading } = useGlobalContext();
+  const url =
+    "https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png";
+  if (loading) {
+    return <div className="loading"></div>;
+  }
+  return (
+    <section className="movies">
+      {movies.map((movie) => {
+        console.log(movie);
+        const { Title: title, Year: year, Poster: poster, imdbID: id } = movie;
+        return (
+          <Link to={`/movies/${id}`} className="movie">
+            <article>
+              <p>{year}</p>
+              <img src={poster === "N/A" ? url : poster} alt={title} />
+            </article>
+          </Link>
+        );
+      })}
+    </section>
+  );
 };
 
 export default Movies;
